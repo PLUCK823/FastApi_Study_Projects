0f3adbbd68c968eec2a6cbc1a40cbcfcf4c41cea
Plucky_Hz <2565796877@qq.com>
2025-08-04 04:14:07 +0800

    feat(auth): 添加用户认证和 token 生成功能
    - 新增用户认证函数 authenticate_user
    - 添加访问令牌生成函数 create_access_token
    - 实现用户密码哈希存储- 新增登录接口 token 生成
    
    Signed-off-by: Plucky_Hz <2565796877@qq.com>

--- project03/routers/auth.py
+++ project03/routers/auth.py
@@ -1,6 +1,10 @@
+from datetime import timedelta, timezone, datetime
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from starlette.exceptions import HTTPException
@@ -10,6 +14,9 @@ from ..database import SessionLocal
 from ..model import Users
 
 router = APIRouter()
+
+SECRET_KEY = "dbaf85d5b99d1203c592d4c9e3d63760abfa4f2198de8d7be41364999f6cd411"
+ALGOTITHM = "HS256"
 
 
 def get_db():
@@ -27,6 +34,22 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+bcrypt_context = CryptContext(schemes=["bcrypt_sha256"], deprecated="auto")
+
+
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.username == username).first()
+    if (not user) or (not bcrypt_context.verify(password, user.hash_password)):
+        return False
+    return user
+
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGOTITHM)
 
 
 class CreateUser(BaseModel):
@@ -51,16 +74,25 @@ class CreateUser(BaseModel):
     }
 
 
-@router.post("/auth", status_code=HTTP_201_CREATED)
+@router.post("/auth/create_user", status_code=HTTP_201_CREATED)
 def create_user(db: db_dependency, user: CreateUser):
     if db.query(Users).filter(Users.username == user.username).first() and db.query(Users).filter(
             Users.email == user.email).first():
         raise HTTPException(status_code=HTTP_409_CONFLICT, detail="User already exists")
     new_user = Users(username=user.username,
-                     hash_password=user.password,
+                     hash_password=bcrypt_context.hash(user.password),
                      email=user.email,
                      first_name=user.first_name,
                      last_name=user.last_name,
                      role=user.role)
     db.add(new_user)
     db.commit()
+
+
+@router.post("/token")
+def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Authenticate Failed"
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    return token
